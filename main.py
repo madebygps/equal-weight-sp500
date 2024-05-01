4d890fb38fb965073cfed6fe41ce78dc3e6b8014
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2024-05-01 02:20:00 +0000

    added alpca calls

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import csv
+import datetime
 import os
+from zoneinfo import ZoneInfo
 import requests
+
+    
+import alpaca
+from alpaca.data.live.stock import *
+from alpaca.data.historical.stock import *
+from alpaca.data.requests import *
+from alpaca.data.timeframe import *
+from alpaca.trading.client import *
+from alpaca.trading.stream import *
+from alpaca.trading.requests import *
+from alpaca.trading.enums import *
+from alpaca.common.exceptions import APIError
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-api_key = os.getenv("API_KEY")
+alpaca_key = os.getenv("ALPACA_KEY")
+alpaca_secret = os.getenv("ALPACA_SECRET")
+data_api_url = None
+
+
+# keys required for stock historical data client
+client = StockHistoricalDataClient(alpaca_key, alpaca_secret)
+# setup stock historical data client
+stock_historical_data_client = StockHistoricalDataClient(alpaca_key, alpaca_secret, url_override = data_api_url)
 
 tickers = []
 with open('tickers.csv', 'r') as file:
@@ -15,9 +37,14 @@ with open('tickers.csv', 'r') as file:
         ticker = str(ticker).replace("'", "").replace("[", "").replace("]", "")
         tickers.append(ticker)
 
-for ticker in tickers[:5]:
-    print(ticker)
-    url = f"https://api.polygon.io/v3/reference/tickers?ticker={ticker}&active=true&apiKey={api_key}"
-    response = requests.get(url)
-    print(response.json())
-    
+now = datetime.now(ZoneInfo("America/New_York"))
+
+for ticker in tickers[:1]:
+    req = StockBarsRequest(
+    symbol_or_symbols = [ticker],
+    timeframe=TimeFrame(amount = 1, unit = TimeFrameUnit.Hour), # specify timeframe
+    start = now - timedelta(days = 5),                          # specify start datetime, default=the beginning of the current day.
+    # end_date=None,                                        # specify end datetime, default=now
+    limit = 1,                                               # specify limit
+    )
+    print(stock_historical_data_client.get_stock_bars(req).df)

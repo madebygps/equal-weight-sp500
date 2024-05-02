2bcb27a4dbd7b0f05b0d52ad36030e69b7bce1bb
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2024-05-02 01:51:36 +0000

    add tickers to cosmos container

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from alpaca.trading.requests import *
 from alpaca.trading.enums import *
 from alpaca.common.exceptions import APIError
 from dotenv import load_dotenv
+from azure.cosmos import CosmosClient
 
 load_dotenv()
 
@@ -24,11 +25,16 @@ alpaca_key = os.getenv("ALPACA_KEY")
 alpaca_secret = os.getenv("ALPACA_SECRET")
 data_api_url = None
 
-
 # keys required for stock historical data client
 client = StockHistoricalDataClient(alpaca_key, alpaca_secret)
 # setup stock historical data client
 stock_historical_data_client = StockHistoricalDataClient(alpaca_key, alpaca_secret, url_override = data_api_url)
+
+# setup cosmos db client
+cosmos_client = CosmosClient(url= os.getenv("COSMOS_DB_ENDPOINT"), credential=os.getenv("COSMOS_DB_KEY"))
+database = cosmos_client.get_database_client(os.getenv("COSMOS_DATABASE"))
+container = database.get_container_client(os.getenv("COSMOS_DB_CONTAINER"))
+
 
 tickers = []
 with open('tickers.csv', 'r') as file:
@@ -39,12 +45,18 @@ with open('tickers.csv', 'r') as file:
 
 now = datetime.now(ZoneInfo("America/New_York"))
 
-for ticker in tickers[:1]:
+for ticker in tickers:
     req = StockBarsRequest(
     symbol_or_symbols = [ticker],
-    timeframe=TimeFrame(amount = 1, unit = TimeFrameUnit.Hour), # specify timeframe
-    start = now - timedelta(days = 5),                          # specify start datetime, default=the beginning of the current day.
-    # end_date=None,                                        # specify end datetime, default=now
-    limit = 1,                                               # specify limit
+    timeframe=TimeFrame.Day, 
+    start = now - timedelta(days = 1),                          # specify start datetime, default=the beginning of the current day.
     )
-    print(stock_historical_data_client.get_stock_bars(req).df)
+    close_price = stock_historical_data_client.get_stock_bars(req).data[ticker][0].close
+    new_stock = {
+        'id': ticker,
+        'ticker': ticker,
+        'close_price': close_price
+    }
+    container.upsert_item(new_stock)
+    print(close_price)
+    
